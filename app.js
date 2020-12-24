2526171ddc59cae10aaef3c84e38b77a0d51590e
Yuvraaj Narula <ynarula678@gmail.com>
2020-12-24 23:16:26 +0530

    nav fixes

---
+++ app.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("clicked");
+  nav.classList.toggle("show");
+});
