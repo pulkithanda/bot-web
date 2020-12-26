cb051f49f9c9dabd29d3658ad61d7e56526f3a8a
Yuvraaj Narula <ynarula678@gmail.com>
2020-12-26 16:18:30 +0530

    command sec

--- app.js
+++ app.js
@@ -21,3 +21,18 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+var button = document.getElementsByClassName('but'),
+    tabContent = document.getElementsByClassName('tab-content');
+button[0].classList.add('active');
+tabContent[0].style.display = 'block';
+
+
+function city(e, city) {
+    var i;
+    for (i = 0; i < button.length; i++) {
+        tabContent[i].style.display = 'none';
+        button[i].classList.remove('active');
+    }
+    document.getElementById(city).style.display = 'block';
+    e.currentTarget.classList.add('active');
+}
